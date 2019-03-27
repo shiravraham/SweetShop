424779f61ea731dfe3a20193bf36b08087f4c732
DESKTOP-7CEDLUI\Shir <shir.avraham13@gmail.com>
2019-03-27 22:50:24 +0200

    cleaned some of the default vs proj, added google map on contact page

--- final-project/wwwroot/js/site.js
+++ final-project/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    var tlv = { lat: 32.0660902, lng: 34.7776924};
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 13.98, center: tlv });
+    var marker = new google.maps.Marker({ position: tlv, map: map });
+});
