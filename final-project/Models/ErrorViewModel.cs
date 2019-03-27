3859cfb558a58996dc803917e2ceff9f0dd835e1
DESKTOP-7CEDLUI\Shir <shir.avraham13@gmail.com>
2019-03-27 21:04:57 +0200

    Add project files.

---
+++ final-project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace final_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
