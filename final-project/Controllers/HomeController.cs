da5540dc0332a28208db3ed9b033c538ae6f8062
DESKTOP-7CEDLUI\Shir <shir.avraham13@gmail.com>
2019-04-30 16:17:14 +0300

    created new controllers and moved files

--- final-project/Controllers/HomeController.cs
+++ final-project/Controllers/HomeController.cs
@@ -19,16 +19,6 @@ namespace final_project.Controllers
             return View();
         }
 
-        public IActionResult SignIn()
-        {
-            return View();
-        }
-
-        public IActionResult SignUp()
-        {
-            return View();
-        }
-
         public IActionResult Shop()
         {
             ViewData["Message"] = "shop message";
