1f93f9739229b5c462acab2e5a3a73ae16bf10ed
DESKTOP-7CEDLUI\Shir <shir.avraham13@gmail.com>
2019-05-27 20:50:45 +0300

    Created admin layout and some basic pages

---
+++ final-project/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using final_project.Models;
+
+namespace final_project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Orders()
+        {
+            return View();
+        }
+        public IActionResult Statistics()
+        {
+            return View();
+        }
+
+        public IActionResult EditProducts()
+        {
+            return View();
+        }
+    }
+}
