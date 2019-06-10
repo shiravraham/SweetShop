e0876d26907ce584168aa92b8ea2c90a55ce84f6
Itay Haizler <itay123987@gmail.com>
2019-06-10 17:29:59 +0300

    add facebook service

--- final-project/wwwroot/js/facebookService.js
+++ final-project/wwwroot/js/facebookService.js
@@ -1,13 +1,13 @@
-    $.getScript('https://connect.facebook.net/en_US/sdk.js', function () {
-        FB.init({
-            appId: '2411340119110577', //app secret 9e97d8d101a13b4bf0ba306d53485b7e
-            version: 'v2.7' // or v2.1, v2.2, v2.3, ...
-        });
-
-        $("#fb-share-button").on('click', function () {
-            FB.ui({
-                method: 'share',
-                href: 'https://www.facebook.com/SweetShop-1043212635885066/?modal=admin_todo_tour',
-            }, function (response) { });
-        })
+$.getScript('https://connect.facebook.net/en_US/sdk.js', function () {
+    FB.init({
+        appId: '2411340119110577', //app secret 9e97d8d101a13b4bf0ba306d53485b7e
+        version: 'v2.7' // or v2.1, v2.2, v2.3, ...
     });
+
+    $("#fb-share-button").on('click', function () {
+        FB.ui({
+            method: 'share',
+            href: 'https://www.facebook.com/SweetShop-1043212635885066/?modal=admin_todo_tour',
+        }, function (response) { });
+    })
+});
