cb22af640fea29ba406d7f3938595b4883738b7a
DESKTOP-7CEDLUI\Shir <shir.avraham13@gmail.com>
2019-07-20 00:37:22 +0300

    removed unnecessary image column

---
+++ final-project/Migrations/20190719213440_RemoveProductImage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace finalproject.Migrations
+{
+    public partial class RemoveProductImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Img",
+                table: "Products");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Img",
+                table: "Products",
+                nullable: true);
+        }
+    }
+}
--- final-project/Models/Product.cs
+++ final-project/Models/Product.cs
@@ -11,6 +11,5 @@ namespace final_project.Models
         public string Name { get; set; }
         public int Price { get; set; }
         public string ImgPath { get; set; }
-        public string Img { get; set; }
     }
 }
