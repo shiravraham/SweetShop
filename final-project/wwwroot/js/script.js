fb954fe63df6abb074f2cc0de150d543cd2e6f6e
DESKTOP-7CEDLUI\Shir <shir.avraham13@gmail.com>
2019-07-20 20:15:27 +0300

    css transitions

--- final-project/wwwroot/js/script.js
+++ final-project/wwwroot/js/script.js
@@ -1,3 +1,17 @@
-﻿$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus');
+﻿$(document).ready(function () {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        ctx.beginPath();
+        ctx.moveTo(1, 1);
+        ctx.lineTo(1, 50);
+        ctx.lineTo(50, 25);
+        ctx.fillStyle = "white";
+        ctx.fill();
+    }
+
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus');
+    });
 });
