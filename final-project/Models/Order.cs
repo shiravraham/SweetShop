69ddc7ee31627fe7fc6a579fbb7a31e6ef0c2c5a
ShakedHad <shakedh100@gmail.com>
2019-07-20 22:29:49 +0300

    Added checkout submit logic and fields validation

--- final-project/Models/Order.cs
+++ final-project/Models/Order.cs
@@ -10,6 +10,25 @@ namespace final_project.Models
         public int Id { get; set; }
         public User User { get; set; }
         public OrderStatus Status { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string Zip { get; set; }
+
+        public string CCName { get; set; }
+
+        public string CCNumber { get; set; }
+
+        public string CCExpiration { get; set; }
+
+        public string CCCvv{ get; set; }
+
         public DateTime OrderDate { get; set; }
 
     }
